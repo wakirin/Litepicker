6100b893b7ae39f61f79ead8e546c9b44327b6ff
Brandon <bmacdonald1986@gmail.com>
2021-11-24 13:58:20 -0700

    Feature: Add functionality for range inputs

--- src/plugins/ranges/index.js
+++ src/plugins/ranges/index.js
@@ -5,35 +5,36 @@ Litepicker.add('ranges', {
     const defaultOptions = {
       position: 'left',
       customRanges: {},
+      rangeInputs: false,
       customRangesLabels: ['Today', 'Yesterday', 'Last 7 Days', 'Last 30 Days', 'This Month', 'Last Month'],
       force: false,
       autoApply: picker.options.autoApply,
     };
     picker.options.ranges = { ...defaultOptions, ...picker.options.ranges };
-
     picker.options.singleMode = false;
 
-    const options = picker.options.ranges;
+    const options =  picker.options.ranges;
 
-    if (!Object.keys(options.customRanges).length) {
-      const date = picker.DateTime();
-
-      const thisMonth = (date) => {
+    const thisMonth = (date) => {
         const d1 = date.clone();
         d1.setDate(1);
         const d2 = new Date(date.getFullYear(), date.getMonth() + 1, 0);
 
         return [d1, d2];
-      };
+    };
 
-      const lastMonth = (date) => {
+    const lastMonth = (date) => {
         const d1 = date.clone();
         d1.setDate(1);
         d1.setMonth(date.getMonth() - 1);
         const d2 = new Date(date.getFullYear(), date.getMonth(), 0);
 
         return [d1, d2];
-      };
+    };
+
+    // Set Default ranges if there are no custom ranges specified in options
+    if (!Object.keys(options.customRanges).length) {
+      const date = picker.DateTime();
 
       options.customRanges = {
         [options.customRangesLabels[0]]: [date.clone(), date.clone()],
@@ -92,6 +93,38 @@ Litepicker.add('ranges', {
 
         block.appendChild(item);
       });
+
+      if(options.rangeInputs){
+
+        const customButton = document.createElement('button');
+        customButton.innerText = "Custom";
+        customButton.tabIndex = ui.dataset.plugins.indexOf('keyboardnav') >= 0 ? 1 : -1;
+
+        const customDiv = document.createElement('div');
+
+        const customInput1 = document.createElement('input');
+        const customInput2 = document.createElement('input');
+
+        customDiv.appendChild(customInput1);
+        customDiv.appendChild(customInput2);
+
+        customButton.addEventListener('click', (e) => {
+            const el = e.target;
+            if (el) {
+                if (customDiv.style.display === "none") {
+                    customDiv.style.display = "block";
+                } else {
+                    customDiv.style.display = "none";
+                }
+              }
+        });
+
+        block.appendChild(customButton);
+
+        const fromInput = document.createElement('input');
+        const toInput = document.createElement('input');
+
+      }
 
       ui.querySelector('.container__main').prepend(block);
     });
